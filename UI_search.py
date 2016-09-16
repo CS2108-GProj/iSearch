611a1a05e86038b840e94ff074c75b2e7ea8e504
Chen Di <sundychenya1018@yahoo.com>
2016-09-16 23:00:47 +0800

    Add feature selection, block search if no feature chosen

--- UI_search.py
+++ UI_search.py
@@ -10,28 +10,55 @@ class UI_class:
 
     def __init__(self, master, search_path):
 
-        # Intialize Features
+        # Features
         self.Color_Histogram = BooleanVar()
 
         # Initialize frame
         self.search_path = search_path
         self.master = master
+        self.init_UI()
+
+        self.master.mainloop()
+
+    # Initialize UI
+    def init_UI(self):
         topframe = Frame(self.master)
         topframe.pack()
 
         # Search Buttons
-        topspace = Label(topframe).grid(row=0, columnspan=2)
         self.bbutton = Button(topframe, text=" Choose an image ", command=self.input_query)
         self.bbutton.grid(row=1, column=1)
         self.cbutton = Button(topframe, text=" Search ", command=self.search)
         self.cbutton.grid(row=1, column=2)
 
         # Features
-        self.CH_check_box = Checkbutton(topframe, text="Color Histogram", variable=self.Color_Histogram, onvalue=True, offvalue=False, command=self.trigger_color_historgram)
-        self.CH_check_box.grid(row = 2, column = 1)
-        downspace = Label(topframe).grid(row=3, columnspan=4)
+        self.CH_check_box = Checkbutton(topframe, text="Color Histogram", variable=self.Color_Histogram, onvalue=True,
+                                        offvalue=False, command=self.trigger_color_historgram)
+        self.CH_check_box.grid(row=2, column=1)
 
-        self.master.mainloop()
+        # Input Image Label
+        self.input_image_label = Label(topframe)
+        self.input_image_label.grid(row=3, column=1)
+
+        # Output Image Label List
+        self.output_img_frame = Frame(self.master)
+        self.output_img_frame.pack()
+
+        self.output_image_label_list = []
+        COLUMNS = 5
+        for index in range(10):
+            r, c = divmod(index, COLUMNS)
+            cur_label = Label(self.output_img_frame)
+            cur_label.grid(row=r, column=c)
+            self.output_image_label_list.append(cur_label)
+
+        # Error Dialog Box
+        self.error_box = Label(topframe)
+        self.error_box.grid(row=4)
+
+        # Logo
+        self.logo = Label(topframe, text="iSearch")
+        self.logo.grid(row=5)
 
 
     # Input query
@@ -42,8 +69,11 @@ class UI_class:
 
     # Call search
     def search(self):
-        self.process_query_img()
-        self.show_results_imgs()
+        if self.Color_Histogram.get():
+            self.process_query_img()
+            self.show_results_imgs()
+        else:
+            self.generate_error("Please choose a feature")
 
 
     # Browse local query image
@@ -57,11 +87,11 @@ class UI_class:
         image_file = Image.open(self.filename)
 
         resized = image_file.resize((100, 100), Image.ANTIALIAS)
-        im = ImageTk.PhotoImage(resized)
-        image_label = Label(self.master, image=im)
-        image_label.pack()
+        img = ImageTk.PhotoImage(resized)
 
-        self.master.mainloop()
+        self.input_image_label.config(image=img)
+        self.input_image_label.image=img
+        self.logo.config(text="Upload 1 image")
 
 
     # Process query image, extract image data according to selected features
@@ -72,37 +102,31 @@ class UI_class:
             self.generate_error("Please choose one feature")
 
 
+    # Show result images
     def show_results_imgs(self):
-        self.result_img_frame = Frame(self.master)
-        self.result_img_frame.pack()
+        self.output_img_frame = Frame(self.master)
+        self.output_img_frame.pack()
 
         # perform the search
         searcher = Searcher("index.csv")
         results = searcher.search(self.queryfeatures)
 
         # show result pictures
-        COLUMNS = 5
         image_count = 0
         for (score, resultID) in results:
             # load the result image and display it
-            image_count += 1
-            r, c = divmod(image_count - 1, COLUMNS)
+
             im = Image.open( self.search_path + "/" + resultID)
             resized = im.resize((100, 100), Image.ANTIALIAS)
             tkimage = ImageTk.PhotoImage(resized)
-            myvar = Label(self.result_img_frame, image=tkimage)
-            myvar.image = tkimage
-            myvar.grid(row=r, column=c)
-
-        self.master.mainloop()
+            self.output_image_label_list[image_count].config(image=tkimage)
+            self.output_image_label_list[image_count].image = tkimage
+            image_count += 1
 
 
     # Display error
     def generate_error(self, message):
-        w = Label(self.master, text=message)
-        w.pack()
-
-        self.master.mainloop()
+        self.error_box.config(text=message)
 
 
     # Process with color histogram
@@ -115,10 +139,11 @@ class UI_class:
         query = cv2.imread(self.filename)
         self.queryfeatures = cd.describe(query)
 
+
     # Test function on feature triggering
     def trigger_color_historgram(self):
         print ">>>>>>> CH feature is ", self.Color_Histogram.get()
 
 
 root = Tk()
-window = UI_class(root,'dataset')
+window = UI_class(root, 'dataset')
